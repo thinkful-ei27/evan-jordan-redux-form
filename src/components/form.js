bc99bd33aee8ab780818156d8c65822d8f9bcbd0
Evan Guirino <etg624@gmail.com>
2019-01-31 11:13:58 -0800

    async form validation

--- src/components/form.js
+++ src/components/form.js
@@ -1,38 +1,53 @@
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
 import './form.css';
-import {required, fiveLong, isNumber} from '../validators'
+import { required, fiveLong, isNumber } from '../validators';
+import Input from './input';
 
 export class Form extends React.Component {
   onSubmit(values) {
-    console.log(values);
+    const url =
+      'https://us-central1-delivery-form-api.cloudfunctions.net/api/report';
+    return fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(res => {
+        if (!res.ok) {
+          return res.json().then(err => Promise.reject(err));
+        }
+        return res.json().then(data => console.log(data));
+      })
+      .catch(err => console.log(err));
   }
   render() {
-    console.log(this.props)
     return (
-      <form>
+      <form onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
         <label htmlFor="trackingNumber">Tracking Number</label>
         <Field
           id="trackingNumber"
           name="trackingNumber"
           type="text"
           className="trackingNumber"
-          component="input"
+          component={Input}
           validate={[required, fiveLong, isNumber]}
         />
         <label htmlFor="issue">What is your deal?!</label>
         <Field name="issue" className="issue" component="select">
-          <option value="Missed">Missed Delivery</option>
-          <option value="Stolen">Stolen Delivery</option>
-          <option value="Late">Late Delivery</option>
+          <option value="not-delivered">My delivery hasn't arrived</option>
+          <option value="wrong-item">The wrong item was delivered</option>
+          <option value="missing-part">Missing Part</option>
+          <option value="damaged">Damaged</option>
+          <option value="other">Other</option>
         </Field>
         <label htmlFor="moreDetails">Provide more details</label>
         <Field
-          component="input"
-          name="moreDetails"
+          component={Input}
+          name="details"
           id="moreDetails"
           type="textArea"
-          className="moreDetails"
+          className="more-details"
         />
         <Field component="button" name="submitButton">
           Submit
